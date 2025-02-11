4d5f7e542e5e80db0a1a254fdafdb5c41a12d2ab
Echa Pangersa Sugianto Oeoen <andyepso@gmail.com>
2025-02-11 23:27:31 +0700

    Revert "fix: add prettier and store parameter type as metadata"

--- src/decorator/api-body-decorator.ts
+++ src/decorator/api-body-decorator.ts
@@ -4,27 +4,25 @@ const bodyMetadataKey = Symbol('body');
 /**
  * The Body decorator is used to inject the request body as a parameter of a route handler.
  *
- * @param classTypeParam The class of the request body.
+ * @param classType The class of the request body.
  * @returns A parameter decorator.
  *
  * @example
- *
- */
+ * 
+*/
 export const Body =
-  (classTypeParam?: new () => any): ParameterDecorator =>
-  (target, propertyKey, parameterIndex) => {
-    const parameterTypes = Reflect.getMetadata('design:paramtypes', target, propertyKey as string);
-    const classType: new () => any = classTypeParam ?? parameterTypes[parameterIndex].prototype.constructor;
-    Reflect.defineMetadata(
-      bodyMetadataKey,
-      {
-        parameterIndex,
-        classType,
-      } as ParameterType,
-      target,
-      propertyKey as symbol,
-    );
-  };
+    (classType: new () => any): ParameterDecorator =>
+    (target, propertyKey, parameterIndex) => {
+        Reflect.defineMetadata(
+            bodyMetadataKey,
+            {
+                parameterIndex,
+                classType,
+            } as ParameterType,
+            target,
+            propertyKey as symbol,
+        );
+    };
 
 /**
  * Gets the parameter metadata for the Body decorator.
@@ -33,5 +31,5 @@ export const Body =
  * @returns The parameter metadata.
  */
 export const getBodyParameter = (target: any, propertyKey: string | symbol): ParameterType => {
-  return Reflect.getMetadata(bodyMetadataKey, target, propertyKey);
+    return Reflect.getMetadata(bodyMetadataKey, target, propertyKey);
 };
